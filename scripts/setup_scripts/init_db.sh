8962b9cb5284119b88e79e89e4ae9094bba9769c
Hunter Price <hunterprice4044@gmail.com>
2022-02-17 21:44:18 -0500

    automated creation of api user and added more docker scripts

---
+++ scripts/setup_scripts/init_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ] || [ -z "$2" ] ; then
+    echo "./init_db [mongo_admin_username] [mongo_admin_password]"
+    exit
+fi
+
+echo "## Creating API Mongo User Account"
+docker exec -it smart-ledger-api-1 bash -c "python db_scripts/create_api_user.py $1 $2"
+
+# TODO - add example data
