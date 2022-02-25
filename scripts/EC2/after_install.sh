3a9c0d5da2335067337862eba845caabd2ea0a49
Andrei Cozma <andreig992@gmail.com>
2022-02-25 15:18:40 -0500

    Quick update/fix

--- scripts/EC2/after_install.sh
+++
@@ -1,5 +0,0 @@
-echo "######## START: after_install.sh  ########"
-
-# TODO: EC2 script
-
-echo "######## DONE : after_install.sh  ########"
--- scripts/EC2/application_start.sh
+++
@@ -1,5 +0,0 @@
-echo "######## START: application_start.sh  ########"
-
-# TODO: EC2 script
-
-echo "######## DONE : application_start.sh  ########"
