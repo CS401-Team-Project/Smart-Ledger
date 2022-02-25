bd4dd8e45d793a07fc0f95f06a68155a124809b2
Andrei Cozma <andreig992@gmail.com>
2022-02-25 15:05:50 -0500

    Added blank after-install and application-start scripts for setup

---
+++ scripts/EC2/after_install.sh
@@ -0,0 +1,5 @@
+echo "######## START: after_install.sh  ########"
+
+# TODO: EC2 script
+
+echo "######## DONE : after_install.sh  ########"
---
+++ scripts/EC2/application_start.sh
@@ -0,0 +1,5 @@
+echo "######## START: application_start.sh  ########"
+
+# TODO: EC2 script
+
+echo "######## DONE : application_start.sh  ########"
