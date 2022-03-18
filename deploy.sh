f9011903cf9074a2f58ded89580d01cd8c1629b4
Andrei Cozma <andreig992@gmail.com>
2022-03-18 16:54:38 -0500

    Quick update/fix

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "# CONTAINERS"
+docker ps
+
+echo
+echo "# DOWN..."
+docker-compose down --rmi=all --volumes --remove-orphans
+
+echo
+echo "# DISK USAGE"
+docker system df
+
+echo
+echo "# CONTAINERS"
+docker container ls
+
+echo
+echo "# IMAGE"
+docker image ls
+
+echo
+echo "# VOLUMES"
+docker volume ls
+
+echo
+echo "# NETWORKS"
+docker network ls
+
+echo
+echo "# CONTAINERS"
+docker ps
+
+echo
+echo "# UP..."
+docker-compose up -d
+
+echo
+echo "# CONTAINERS"
+docker ps
