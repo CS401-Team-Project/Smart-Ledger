ad259e8217764897a5087398de75e9ce23adc152
Andrei Cozma <me@andreicozma.com>
2022-03-18 22:16:40 -0400

    Quick update/fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+BRANCH=$1
+
+if [ ! -z "$BRANCH" ]; then
+	set +v
+	echo "# Front-End Checkout: $BRANCH"
+	cd Front-End && git checkout "$BRANCH" || exit 1
+	echo "# Front-End Pull: $BRANCH"
+	git pull && cd .. || exit 1
+	
+	echo "# Back-End Checkout: $BRANCH"
+	cd Back-End && git checkout "$BRANCH" || exit 1
+	echo "# Back-End Pull: $BRANCH"
+	git pull && cd .. || exit 1
+	
+	echo "# GIT Add Front-End & Back-End"
+	git add Front-End && git add Back-End || exit 1
+
+	echo "# GIT Push"
+	git commit -m "Deploy to EC2: $BRANCH" && git push || exit 1
+	exit 0
+fi
+
 echo "# CONTAINERS"
 docker ps
 
