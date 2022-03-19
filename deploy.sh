19056a0c11b064898ea26fe0409654574ecfc391
Andrei Cozma <me@andreicozma.com>
2022-03-18 23:17:18 -0400

    Quick update/fix

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 function cleanreset () {
-	echo
+	echo "====================================================================="
 	echo "====================================================================="
 	echo "# GIT Config"
 	git config --global submodule.recurse true
 	git config --global status.submoduleSummary true
 
-	echo
 	echo "====================================================================="
 	echo "# GIT Status"
 	git status
 
-	echo
 	echo "====================================================================="
 	echo "# GIT Clean Reset"
 	git clean -xfd
@@ -21,20 +19,21 @@ function cleanreset () {
 	git submodule foreach --recursive git reset --hard
 	git submodule update --init --recursive
 
-	echo
 	echo "====================================================================="
 	echo "# GIT Checkout Main"
 	git checkout main
 
-	echo
 	echo "====================================================================="
 	echo "# GIT Pull"
 	git pull
 
-	echo
 	echo "====================================================================="
 	echo "# GIT Status"
 	git status
+
+	echo
+	echo "====================================================================="
+	echo "====================================================================="
 }
 
 if [ "$1" == "clean-reset" ]; then
@@ -43,81 +42,83 @@ if [ "$1" == "clean-reset" ]; then
 else
 	if [ ! -z "$1" ]; then
 		BRANCH=$1
+		echo "====================================================================="
+		echo "====================================================================="
 		set +v
 		echo "# Front-End Checkout: $BRANCH"
-		cd Front-End && git checkout "$BRANCH" || exit 1
+		cd Front-End && git status && git checkout "$BRANCH" || exit 1
 		echo "# Front-End Pull: $BRANCH"
-		git pull && cd .. || exit 1
+		git pull && git status && cd .. || exit 1
 
 		echo "# Back-End Checkout: $BRANCH"
-		cd Back-End && git checkout "$BRANCH" || exit 1
+		cd Back-End && git status && git checkout "$BRANCH" || exit 1
 		echo "# Back-End Pull: $BRANCH"
-		git pull && cd .. || exit 1
+		git pull && git status && cd .. || exit 1
 
 		echo "# GIT Add Front-End & Back-End"
 		git add Front-End && git add Back-End || exit 1
 
 		echo "# GIT Push"
 		git commit -m "Deploy to EC2: $BRANCH" && git push || exit 1
+		set -v
+		echo "====================================================================="
+		echo "====================================================================="
 		exit 0
 	fi
 fi
 
 function currstate () {
 	echo "====================================================================="
+	echo "====================================================================="
 	echo "# DISK USAGE"
 	docker system df
 
-	echo
-	echo "====================================================================="
+	echo "---------------------------------------------------------------------"
 	echo "# CONTAINERS"
 	docker container ls
 
-	echo
-	echo "====================================================================="
+	echo "---------------------------------------------------------------------"
 	echo "# IMAGE"
 	docker image ls
 
-	echo
-	echo "====================================================================="
+	echo "---------------------------------------------------------------------"
 	echo "# VOLUMES"
 	docker volume ls
 
-	echo
-	echo "====================================================================="
+	echo "---------------------------------------------------------------------"
 	echo "# NETWORKS"
 	docker network ls
 
-	echo
-	echo "====================================================================="
+	echo "---------------------------------------------------------------------"
 	echo "# CONTAINERS"
 	docker ps
 
-	echo
+	echo "====================================================================="
 	echo "====================================================================="
 }
 
-
-echo "====================================================================="
 currstate
 
 echo
+echo "====================================================================="
 echo "====================================================================="
 echo "# DOWN..."
 docker-compose down --rmi=all --volumes --remove-orphans
 
-echo
-echo "====================================================================="
+echo "---------------------------------------------------------------------"
 echo "# PRUNE..."
 docker system prune -af
-
 echo "====================================================================="
+echo "====================================================================="
+echo
+
 currstate
 
 echo
 echo "====================================================================="
+echo "====================================================================="
 echo "# UP..."
 docker-compose up -d || exit 1
-
+echo "====================================================================="
 echo "====================================================================="
 currstate
